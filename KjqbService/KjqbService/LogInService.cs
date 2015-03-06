b0f0438d445184b73caa6ae75afaeaa6a4f0c2a9
Mashi <mashi596@hotmail.com>
2015-03-06 15:57:11 +0800

    服务端程序

---
+++ KjqbService/KjqbService/LogInService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KjqbService
+{
+    public class LogInService
+    {
+        private long writeUserId;
+
+        public long WriteUserId
+        {
+            get { return writeUserId; }
+            set { writeUserId = value; }
+        }
+        private long shareUserId;
+
+        public long ShareUserId
+        {
+            get { return shareUserId; }
+            set { shareUserId = value; }
+        }
+        private long logId;
+
+        public long LogId
+        {
+            get { return logId; }
+            set { logId = value; }
+        }
+
+    }
+}
