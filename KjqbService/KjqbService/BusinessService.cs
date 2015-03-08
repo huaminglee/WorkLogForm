56c57a0d7c560647c8ade41a5c83464ecd2ea6ae
ccc65535 <yue.charles@tom.com>
2015-03-08 10:50:57 +0800

    数据库中出差表

--- KjqbService/KjqbService/BusinessService.cs
+++ KjqbService/KjqbService/BusinessService.cs
@@ -8,8 +8,46 @@ namespace KjqbService
     public class BusinessService
     {
         private long staterID;
+
+        public long StaterID
+        {
+            get { return staterID; }
+            set { staterID = value; }
+        }
+
+
         private long receiveID;
+
+        public long ReceiveID
+        {
+            get { return receiveID; }
+            set { receiveID = value; }
+        }
+
+
         private long businessID;
 
+        public long BusinessID
+        {
+            get { return businessID; }
+            set { businessID = value; }
+        }
+
+
+        private long timeStamp;
+
+        public long TimeStamp
+        {
+            get { return timeStamp; }
+            set { timeStamp = value; }
+        }
+
+        private int type;
+
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
     }
 }
---
+++ KjqbService/KjqbService/DB/BusinessOperate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KjqbService.DB
+{
+    public class BusinessOperate:DBoperate
+    {
+        public int InsertIntoEntity(BusinessService b)
+        {
+            return 1;
+        }
+    }
+}
