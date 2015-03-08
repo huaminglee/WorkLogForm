8d5214c6a4d00d9c9c8d04963029bd3437913bb5
ccc65535 <yue.charles@tom.com>
2015-03-08 10:02:46 +0800

    出差推送1

---
+++ KjqbService/KjqbService/BusinessService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KjqbService
+{
+    public class BusinessService
+    {
+        private long staterID;
+        private long receiveID;
+        private long businessID;
+
+    }
+}
